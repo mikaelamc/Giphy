99037dbfb7b5af6af024ad77e207037671f8d8b6
Mikaela Mckee <mikaelamckee@iMac.local>
2018-07-20 21:39:02 -0500

    second commit

---
+++ Assets/JavaScript/giphy.js
@@ -0,0 +1,59 @@
+var topic = [
+    "Australia",
+    "USA",
+    "Brazil",
+    "Italy"
+];
+
+$('button').on('click', function() {
+  console.log("Clicked Button");
+  
+})
+    var topic= $(this).attr("data-search");
+   
+   
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +topic+ "&api_key=I7ZUNNcBhD24mzmGIIKYcyxi0HBlBxx7&limit=10";
+    console.log(queryURL);
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+
+    })
+      .then(function(response){ 
+        console.log(response);
+     
+        for (var i=0; i <response.data.length; i++) {
+        console.log(response.data[0].rating);
+
+        var topicDiv = $('<div>');
+        var p = $('<p>').text("Rating: " + response.data[i].rating + "</p>");
+        var topicImage = $('<img>');
+        topicImage.attr('src', response.data[i].images.fixed_height.url);
+        topicDiv.append(p);
+        topicDiv.append(topicImage);
+        $('#gifsGohere').append(topicDiv);
+      
+
+          // $('body').append("<p>Rating: " + response.data[0].rating + "</p>");
+          // $('body').append("<img src=' " +response.data[0].images.downsized.url +"'>");
+          
+        
+        }
+      })
+          
+      
+ 
+
+
+
+//Notes for Starting and Stoping the Gif
+
+    // if (topic === "still") {
+    //   $(this).attr("src", $(this).attr("data-animate"));
+    //   $(this).attr("data-state", "animate");
+    // } else {
+    //   $(this).attr("src", $("this").attr("data-still"));
+    //   $(this).attr("data-state", "still");
+    // }
+
